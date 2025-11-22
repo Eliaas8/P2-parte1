4a70a743888fa928c57a41b530b08f65db78cf27
Eliaas8 <eggm0108@gmail.com>
2025-11-22 18:41:33 -0500

    update -e
    .

--- include/Producto.h
+++ include/Producto.h
@@ -8,12 +8,12 @@
 class Producto {
 
 private:
-    // [ELIAS - TODO #4] Declara el atributo: 'id' (int).
-    // [ELIAS - TODO #5] Declara el atributo: 'nombre' (std::string).
-    // [ELIAS - TODO #6] Declara el atributo: 'precio' (float o double).
-    // [ELIAS - TODO #7] Declara el atributo: 'categoria' (std::string).
-    // [ELIAS - TODO #8] Declara el atributo: 'stock' (int).
-    // [ELIAS - TODO #9] Declara el atributo: 'calificacionPromedio' (float).
+    int id;// [ELIAS - TODO #4] Declara el atributo: 'id' (int).
+    std::string nombre// [ELIAS - TODO #5] Declara el atributo: 'nombre' (std::string).
+    double precio;// [ELIAS - TODO #6] Declara el atributo: 'precio' (float o double).
+    std::string categoria;// [ELIAS - TODO #7] Declara el atributo: 'categoria' (std::string).
+    int stock;// [ELIAS - TODO #8] Declara el atributo: 'stock' (int).
+    float clasificacionPromedio// [ELIAS - TODO #9] Declara el atributo: 'calificacionPromedio' (float).
 
 public:
     Producto(int id, std::string nombre, double precio, std::string categoria, int stock, float calificacionPromedio);
