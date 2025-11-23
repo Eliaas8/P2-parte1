fd3e07687f0a91a4c78785297dee12f4ffd18ae8
Eliaas8 <eggm0108@gmail.com>
2025-11-22 19:08:18 -0500

    Implement constructors and getters in Producto class
    -e

--- src/Producto.cpp
+++ src/Producto.cpp
@@ -1,24 +1,40 @@
 #include "Producto.h"
 #include <iostream>
 
-// [ELIAS - TODO #16] Implementa el Constructor con Parámetros.
-// Usa 'this->variable = variable' o una lista de inicialización para asignar los valores.
 Producto::Producto(int id, std::string nombre, double precio, std::string categoria, int stock, float calificacionPromedio) {
-    // Asigna los argumentos a los atributos privados aquí
+    
+    id(id);
+    nombre(nombre);
+    pecio(pecio);
+    categoria(categoria);
+    stock(stock);
+    calificacionPromedio(calificacionPromedio);
 }
 
-// [ELIAS - TODO #17] Implementa el Constructor Por Defecto.
-// Inicializa los números en 0 y los strings en vacío o "Desconocido".
 Producto::Producto() {
-    // Lógica aquí
+   
+    id(0);
+    nombre("Desconocido");
+    pecio(0.0);
+    categoria("Desconocido");
+    stock(0);
+    calificacionPromedio(0.0f);
 }
 
-// [ELIAS - TODO #18] Implementa todos los métodos Getters definidos en el header.
-// Ejemplo: int Producto::getId() const { return id; }
-// Haz esto para: id, nombre, precio, categoria, stock, calificacionPromedio.
+int Producto::getId() const { return id; }
+std::string Producto::getNombre() const { return nombre; }
+double Producto::getPrecio() const { return precio; }
+std::string Producto::getCategoria() const { return categoria; }
+int Producto::getStock() const { return stock; }
+float Producto::getCalificacionPromedio() const { return calificacionPromedio; }
 
-// [ELIAS - TODO #19] Implementa 'mostrarInfo()'.
-// Usa std::cout para imprimir todos los atributos en un formato legible.
 void Producto::mostrarInfo() const {
-    // cout << "ID: " << id << " Nombre: " << nombre ... etc
+
+    std::cout << "ID: " << id << "\n";
+    std::cout << "Nombre: " << nombre << "\n";
+    std::cout << "Precio: $" << precio << "\n";
+    std::cout << "Categoría: " << categoria << "\n";
+    std::cout << "Stock: " << stock << "\n";
+    std::cout << "Calificación Promedio: " << calificacionPromedio << "\n"; 
+    
 }
